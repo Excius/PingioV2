99bd67754fe73d4618e0181b76572632de85f06e
Excius <rao.daksh555@gmail.com>
2025-07-27 16:08:38 +0530

    feat: initialize frontend with Vite and React setup
    - Added package.json for project dependencies and scripts
    - Included Vite logo SVG in public directory
    - Created App.css for styling the application
    - Developed main App component with basic functionality
    - Added React logo SVG in assets directory
    - Established global styles in index.css
    - Set up main entry point in main.tsx
    - Defined Vite environment types in vite-env.d.ts
    - Configured TypeScript settings in tsconfig files
    - Created Vite configuration file for project setup

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth", authRoutes);
+
+async function startServer() {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+startServer();
---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlenght: 6,
+    },
+    profilePic: {
+      type: String,
+      default:
+        "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
